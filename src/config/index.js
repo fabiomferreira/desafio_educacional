83e730eda36da5556370d41e82b133924402f1a2
Fabio Ferreira <fabiomf@protonmail.com>
2020-03-01 23:32:11 -0400

    Fix production API URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  "API_URL": process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://desafio-educacional.herokuapp.com/',
+  "API_URL": process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://desafio-educacional.herokuapp.com',
 } 
 
 module.exports = config;
