8fa225a1a8d316de383308535c5946e96b379963
Fabio Ferreira <fabiomf@protonmail.com>
2020-03-01 22:40:35 -0400

    Updates

---
+++ precache-manifest.fc536af7b9c86d35143b102bcbde2027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2427814e5c43bce62034c9531857037b",
+    "url": "/desafio_educacional/index.html"
+  },
+  {
+    "revision": "02b2f2a16ab0b9097abf",
+    "url": "/desafio_educacional/static/css/main.cfdc2d8d.chunk.css"
+  },
+  {
+    "revision": "716633e89c497283a7df",
+    "url": "/desafio_educacional/static/js/2.5201403d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/desafio_educacional/static/js/2.5201403d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b2f2a16ab0b9097abf",
+    "url": "/desafio_educacional/static/js/main.46bb5870.chunk.js"
+  },
+  {
+    "revision": "022d89db2620e975b2e4",
+    "url": "/desafio_educacional/static/js/runtime-main.9db12e91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e4345298c092935a575c74e559bcb3c.js"
+  "/desafio_educacional/precache-manifest.fc536af7b9c86d35143b102bcbde2027.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/desafio_educacional/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
