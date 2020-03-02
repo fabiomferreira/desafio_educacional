7a9fabc35bc4b99b7989ec246a991cb45e33b444
Fabio Ferreira <fabiomf@protonmail.com>
2020-03-01 22:29:47 -0400

    Updates

---
+++ precache-manifest.5e4345298c092935a575c74e559bcb3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4932877acaafb441f5a1195566bf9710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8aefbce8b8fb38c844",
+    "url": "/static/css/main.cfdc2d8d.chunk.css"
+  },
+  {
+    "revision": "d5352f791cfc28222fc2",
+    "url": "/static/js/2.87531076.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.87531076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8aefbce8b8fb38c844",
+    "url": "/static/js/main.de76a923.chunk.js"
+  },
+  {
+    "revision": "77be7bc9c599184406a7",
+    "url": "/static/js/runtime-main.980454a5.js"
+  }
+]);
